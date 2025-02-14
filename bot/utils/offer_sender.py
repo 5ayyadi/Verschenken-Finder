35e25d42c261d085d2e11d04172dd825a7da432e
mahmoud <eslamimahmoud98@gmail.com>
2025-02-14 11:38:16 +0100

    feat: format the posts

--- bot/utils/offer_sender.py
+++ bot/utils/offer_sender.py
@@ -6,31 +6,56 @@ from core.constants import TOKEN
 
 bot = Bot(token=TOKEN)
 
+
+# JSON attributes:
+# - id
+# - title
+# - description
+# - address
+# - link
+# - offer_date
+# - photos (list)
+# - location (dict: city_id, city_name, state_id, state_name)
+# - category (dict: category_id, category_name, subcategory_id, subcategory_name)
+# - telegram_post_id
+
 async def send_offer_to_user(user_id: str, offer: Offer):
     """
-        This function will send the offer to the user.
+        This function sends a formatted offer message to the user.
     """
-    message = offer.model_dump_json()
-    await bot.send_message(chat_id=user_id, text=message)
-    
+    offer_data = offer.model_dump()
+
+    message = (
+        f"<b>{offer_data['title']}</b>\n\n"
+        f"📝 <b>Beschreibung:</b>\n{offer_data['description']}\n\n"
+        f"📍 <b>Ort:</b> {offer_data['address']} ({offer_data['location']['state_name']})\n"
+        f"🗂️ <b>Kategorie:</b> {offer_data['category']['subcategory_name']} ({offer_data['category']['category_name']})\n"
+        f"📅 <b>Datum:</b> {offer_data['offer_date']}\n\n"
+        f"🔗 <a href='{offer_data['link']}'>Mehr Details</a>\n"
+    )
+
+    if 'photos' in offer_data and offer_data['photos']:
+        message += f"🖼️ <a href='{offer_data['photos'][0]}'>Bild ansehen</a>\n"
+
+    await bot.send_message(chat_id=user_id, text=message, parse_mode="HTML")
+
 
 async def send_offers(pref_dict: dict):
     """
         This function will be called by the celery worker to send
         the offers to the users.
     """
-    
+
     filter_criteria = {
         "location.city_id": pref_dict["city_id"],
         "location.state_id": pref_dict["state_id"],
         "category.category_id": pref_dict["category_id"],
         "category.subcategory_id": pref_dict["sub_category_id"]
     }
-    offers = MongoDBClient.get_offers(filter_criteria)
-    
+    offers = list(reversed(MongoDBClient.get_offers(filter_criteria)))
+
     for user_id in pref_dict.get("users"):
         sent_offers = RedisClient.get_sent_offer_ids(user_id)
-        
 
         for offer in offers:
             if offer.id not in sent_offers:
